d18d7074fdc344abafc49efd57ac22bac77f5fff
cjoynerw <cjoynerw@gmail.com>
2020-12-10 13:26:49 -0600

    created initialize game function to reset values when new game button is clicked

--- script.js
+++ script.js
@@ -13,10 +13,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
 // Reused functions
 const switchPLayer = function () {
@@ -27,10 +24,24 @@ const switchPLayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// Starting game
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 // Roll dice functionality
 btnRoll.addEventListener('click', function () {
@@ -67,6 +78,7 @@ btnHold.addEventListener('click', function () {
     if (scores[activePlayer] >= 10) {
       // Finish the game
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -79,3 +91,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
+
+init();
